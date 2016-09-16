c359dbd131a91746b3490b8029813b7730eb498a
SergeGaliullin <sergei.galiullin@gmail.com>
2016-09-16 14:00:47 -0300

    Account configurations in settings.py

--- ds/ds/settings.py
+++ ds/ds/settings.py
@@ -76,6 +76,11 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
